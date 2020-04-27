1f2a3d4f6820bead0f6d68a78ce917c163fa7400
Dan Weikart <danmweikart@gmail.com>
2020-04-27 13:31:09 -0700

    Added sample questions, created html template.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 
 inquirer.prompt([
-    // {
-    //   type: "input",
-    //   name: "name",
-    //   message: "What is your name?"
-    // },
-    // {
-    //     type: "input",
-    //     name: "name",
-    //     message: "What is your name?"
-    //   },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description."
+      },
     //   {
     //     type: "input",
     //     name: "name",
