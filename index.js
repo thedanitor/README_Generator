420435b50435f575c87b9121036bb55f54e74450
Dan Weikart <danmweikart@gmail.com>
2020-04-27 11:12:12 -0700

    Added inquirer.promt.

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 
-
+inquirer.prompt([
+    // {
+    //   type: "input",
+    //   name: "name",
+    //   message: "What is your name?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your name?"
+    //   }
+    ]);
 
 const questions = [
 
