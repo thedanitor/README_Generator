2929be4d5506f5bc623021a2647778f802977761
Dan Weikart <danmweikart@gmail.com>
2020-05-02 15:08:13 -0700

    Added comment to js.

--- index.js
+++ index.js
@@ -1,62 +1,56 @@
 var inquirer = require("inquirer");
-var fs = require('fs');
-var axios =require('axios');
+var fs = require("fs");
+var axios = require("axios");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
   {
-  type: "input",
-  name: "title",
-  message: "What is the name of your project?"
-},
-{
-  type: "input",
-  name: "url",
-  message: "What is the URL to the repository?"
-},
-{
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "url",
+    message: "What is the URL to the repository?",
+  },
+  {
     type: "input",
     name: "description",
-    message: "Write a brief description of the project."
+    message: "Write a brief description of the project.",
   },
   {
     type: "input",
     name: "github",
-    message: "What is your Github username?"
+    message: "What is your Github username?",
   },
   {
     type: "input",
     name: "email",
-    message: "What is your email?"
+    message: "What is your email?",
   },
   {
     type: "list",
     message: "What kind of license do you want your project to have?",
     name: "license",
-    choices: [
-      "MIT",
-      "GNU GPLv3",
-      "WTFPL",
-      "Unlicense",
-      "None"
-    ]
+    choices: ["MIT", "GNU GPLv3", "WTFPL", "Unlicense", "None"],
   },
   {
     type: "input",
     name: "usage",
-    message: "How does the user use this project?"
+    message: "How does the user use this project?",
   },
   {
     type: "input",
     name: "install",
     message: "What commands would the user need to intall this project?",
-    default: "npm i"
+    default: "npm i",
   },
   {
     type: "input",
     name: "tests",
     message: "How would the user test this project?",
-    default: "npm test"
+    default: "npm test",
   },
   {
     type: "input",
@@ -66,39 +60,38 @@ const questions = [
   {
     type: "input",
     name: "credits",
-    message: "Enter any acknowledgements here."
-  }
+    message: "Enter any acknowledgements here.",
+  },
 ];
 
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, err => {
+  fs.writeFile(fileName, data, (err) => {
     if (err) {
       throw err;
-    } 
-    console.log("ReadMe created successfully!");
+    }
+    console.log("README created successfully!");
   });
-};
+}
 
 function init() {
-  inquirer.prompt(questions).then(answers => {
-  
-    let data = {...answers}
+  inquirer.prompt(questions).then((answers) => {
+    let data = { ...answers };
 
-    const queryUrl = `https://api.github.com/users/${answers.github}`; 
+    const queryUrl = `https://api.github.com/users/${answers.github}`;
     console.log(queryUrl);
 
-    axios.get(queryUrl).then(function(response) {
-      data.photo = response.data.avatar_url;
-      console.log(`Combined inputs` , data);
-      const markdown = generateMarkdown(data);
-      writeToFile("README.md", markdown);
-  
-    })
-    .catch(err => {
-      console.log("Github user not found");
-    })
-  }) 
-
+    axios
+      .get(queryUrl)
+      .then(function (response) {
+        data.photo = response.data.avatar_url;
+        console.log(`Combined inputs`, data);
+        const markdown = generateMarkdown(data);
+        writeToFile("README.md", markdown);
+      })
+      .catch((err) => {
+        console.log("Github user not found");
+      });
+  });
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,30 @@
-
-function licenseBadge (data) {
+// Create badge depending on license type selected
+function licenseBadge(data) {
   if (data.license === "MIT") {
     data.licenseType = "This project is licensed under the MIT license";
-    data.badges = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-  } else if(data.license === "GNU GPLv3") {
+    data.badges =
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (data.license === "GNU GPLv3") {
     data.licenseType = "This project is licensed under the GNU GPLv3 license";
-    data.badges = "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)"
-  } else if(data.license === "WTFPL") {
+    data.badges =
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)";
+  } else if (data.license === "WTFPL") {
     data.licenseType = "This project is licensed under the WTFPL license";
-    data.badges = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)"
-  } else if(data.license === "Unlicense") {
+    data.badges =
+      "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+  } else if (data.license === "Unlicense") {
     data.licenseType = "This project is licensed under the Unlicense license";
-    data.badges = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+    data.badges =
+      "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
   } else {
     data.licenseType = "This project has no license information";
-    data.badges = "[![Generic badge](https://img.shields.io/badge/License-NoLicense-red.svg)](https://shields.io/)"
+    data.badges =
+      "[![Generic badge](https://img.shields.io/badge/License-NoLicense-red.svg)](https://shields.io/)";
   }
 }
 
 function generateMarkdown(data) {
-
   licenseBadge(data);
-
   return `
 # ${data.title}
 
